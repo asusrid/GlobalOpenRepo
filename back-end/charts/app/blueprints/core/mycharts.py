6cd80fab9b96f400bc7c52acf94b1aa25aa5def5
asusrid <32960104+asusrid@users.noreply.github.com>
2020-03-29 12:35:44 -0500

    Line plot implemented
    waiting for arguments to total functionallity

--- back-end/charts/app/blueprints/core/mycharts.py
+++ back-end/charts/app/blueprints/core/mycharts.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import io
+
+from pandas.plotting import register_matplotlib_converters
 from sklearn.datasets import load_breast_cancer
 
 
@@ -31,3 +33,35 @@ def makePlot():
     plt.savefig(bytes_image, format='png')
     bytes_image.seek(0)
     return bytes_image
+
+
+def linePlot(x, y, fileName, todoNaN):
+
+    #warehouse/serie_historica_acumulados.csv
+    # be aware of the array fileName in case we have more than one file
+    data_spain_ccaa = pd.read_csv('/Users/alejandrosusillo/Downloads/serie_historica_acumulados.csv', sep=',') 
+
+    # ASK THE USER WHAT TO DO WITH CERTAIN RECORDS!!!!!!!!!!!!!!!!!!!!!!!!!!
+    data_spain_ccaa = data_spain_ccaa.drop(len(data_spain_ccaa)-1)
+    # ASK THE USER WHAT TO DO WITH THE NAN VALUES
+    data_spain_ccaa['Casos '] = data_spain_ccaa['Casos '].fillna(0)
+
+    # get andalucia cases
+    aux = data_spain_ccaa[data_spain_ccaa['CCAA Codigo ISO'].str.contains('AN')]
+    # get those days that are even
+    even_days = (pd.to_datetime(aux['Fecha']).dt.day)%2==0
+    # get those records that are AN and even days
+    data_spain_ccaa_AN = aux[aux['CCAA Codigo ISO'].str.contains('AN')][even_days]
+
+
+    fig, ax = plt.subplots()
+
+    ax.plot_date(data_spain_ccaa_AN['Fecha'], data_spain_ccaa_AN['Casos '], marker='', linestyle='-')
+
+    fig.autofmt_xdate()
+
+    #plt.plot(data_spain_ccaa_AN['Fecha'], data_spain_ccaa_AN['Casos '])
+    plt.ylabel('Cases')
+    plt.xlabel('Date')
+    plt.show()
+
